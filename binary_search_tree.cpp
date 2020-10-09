88d1422cb29246a3564e107a6324e793bbe1458f
Vaibhav Chhabra <vaibhavarena@gmail.com>
2020-10-09 22:30:54 +0530

    Floor and ceil

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -156,13 +156,73 @@ Node* delNode(Node* root, int x)
 	}
 }
 
+Node* floor(Node* root, int x)
+{
+	Node* res = NULL;
+	while (root != NULL)
+	{
+		if (root->key == x)
+			return root;
+		else if (root->key > x)
+		{ 
+			res = root;
+			root = root->left;
+		}
+			
+		else
+			root = root->right;
+	}
+	return res;
+}
+
+Node* ceil(Node* root, int x)
+{
+	Node* res = NULL;
+	while (root != NULL)
+	{
+		if (root->key == x)
+			return root;
+		else if (root->key > x)
+			root = root->left;
+		else {
+			res = root;
+			root = root->right;
+		}
+	}
+	return res;
+}
+
+int minValue(Node* root)
+{
+	// Code here
+	if (root == NULL)
+		return -1;
+	int min;
+	Node* cur = root;
+	min = cur->key;
+	while (cur->left != NULL || cur->right != NULL)
+	{
+		if (cur->left != NULL)
+		{
+			cur = cur->left;
+			min = cur->key;
+		}
+		else
+		{
+			cur = cur->right;
+			min = cur->key;
+		}
+	}
+	return min;
+}
+
 int main()
 {
 	Node* root = new Node(50);
 	insert(root, 20);
 	insert(root, 30);
 	insert(root, 40);
-	insert(root, 50);
+	insert(root, 10);
 	insert(root, 60);
 	insertIter(root, 70);
 	insertIter(root, 80);
@@ -171,12 +231,29 @@ int main()
 	insertIter(root, 65);
 	insertIter(root, 55);
 
+	//				50
+	//			  /	   \
+	//			 /	    \
+	//			/	     \
+	//		  20		  60
+	//		/   \			\
+	//	   10	30			70
+	//			 \		   /   \
+	//			 40		  65	80
+	//			/  \	  /	
+	//		   35   45   55
+
 	std::cout << "Level order traversal : \n";
 	traverse(root);
 	std::cout << "\nSearching for node -> ";
 	std::cout << "\nWhen node is present : " << search(root, 40) << "\nWhen node is not present : " << searchIter(root, 100);
 	std::cout << "\n\nDelete a node -> : ";
 	std::cout << "\nWhen a node is present : " << delNode(root, 40) << "\nWhen a node is not present : " << typeid(delNode(root, 100)).name();
-
+	std::cout << "\nFloor in a BST : ";
+	std::cout << floor(root, 56)->key;
+	std::cout << "\nCeil in a BST : ";
+	std::cout << ceil(root, 56)->key;
+	std::cout << "\nMin value : ";
+	std::cout << minValue(root);
 	return 0;
 }
